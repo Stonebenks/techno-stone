abffd69906638902a5cbb53493976dcd16a40c07
Stonebenks <139616092+Stonebenks@users.noreply.github.com>
2023-11-23 12:14:51 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,19 @@
+let flightEl = document.getElementById(flight-el)
+let hotelEl = document.getElementById( hotel-el)
+let rentEl = document.getElementById(rent-el)
+
+
+console.log(rentEl)
+
+
+
+var quategori1s = document.getElementsByClassName("quategori1")
+
+var quategoris = document.getElementsByClassName("bble")
+
+function opentab(tabname){
+    for(quategori1s of quategori1){
+        quategori1s.cla
+    }
+}
+console.log(quategoris)
